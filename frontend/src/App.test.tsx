152f1425d2f39369e7d4809649b0762af55a641a
Yasuaki Iwata <yasuaki.iwata0101@gmail.com>
2026-02-27 22:27:51 +0900

    feat: add timeline screen

---
+++ frontend/src/App.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import App from './App'
+
+describe('App', () => {
+  it('投稿フォームが表示される', () => {
+    render(<App />)
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: '投稿' })).toBeInTheDocument()
+  })
+
+  it('新しい投稿を追加するとタイムラインに表示される', async () => {
+    render(<App />)
+    await userEvent.type(screen.getByRole('textbox'), '新しい投稿')
+    await userEvent.click(screen.getByRole('button', { name: '投稿' }))
+    expect(screen.getByText('新しい投稿')).toBeInTheDocument()
+  })
+})
