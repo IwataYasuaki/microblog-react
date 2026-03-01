9aa2f160bbfb2adefe506cecca9b10b1f227fdab
Yasuaki Iwata <yasuaki.iwata0101@gmail.com>
2026-03-02 02:01:56 +0900

    feat: add CDK infra stack with DynamoDB, Lambda, and API Gateway

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,16 +1,77 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib/core'
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as apigateway from 'aws-cdk-lib/aws-apigateway'
+import * as path from 'path'
+import { Construct } from 'constructs'
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
+    // DynamoDB
+    const table = new dynamodb.Table(this, 'PostsTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Lambda共通設定
+    const lambdaProps = {
+      runtime: lambda.Runtime.NODEJS_22_X,
+      code: lambda.Code.fromAsset(path.join(__dirname, '../../backend/dist')),
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+    }
+
+    // Lambda関数
+    const listPostsFn = new lambda.Function(this, 'ListPostsFunction', {
+      ...lambdaProps,
+      handler: 'handlers/posts/list.handler',
+    })
+
+    const createPostFn = new lambda.Function(this, 'CreatePostFunction', {
+      ...lambdaProps,
+      handler: 'handlers/posts/create.handler',
+    })
+
+    const deletePostFn = new lambda.Function(this, 'DeletePostFunction', {
+      ...lambdaProps,
+      handler: 'handlers/posts/delete.handler',
+    })
+
+    const likePostFn = new lambda.Function(this, 'LikePostFunction', {
+      ...lambdaProps,
+      handler: 'handlers/posts/like.handler',
+    })
+
+    const unlikePostFn = new lambda.Function(this, 'UnlikePostFunction', {
+      ...lambdaProps,
+      handler: 'handlers/posts/unlike.handler',
+    })
+
+    // DynamoDBアクセス権限
+    table.grantReadData(listPostsFn)
+    table.grantWriteData(createPostFn)
+    table.grantWriteData(deletePostFn)
+    table.grantReadWriteData(likePostFn)
+    table.grantReadWriteData(unlikePostFn)
+
+    // API Gateway
+    const api = new apigateway.RestApi(this, 'MicroblogApi', {
+      restApiName: 'Microblog API',
+    })
+
+    const posts = api.root.addResource('posts')
+    posts.addMethod('GET', new apigateway.LambdaIntegration(listPostsFn))
+    posts.addMethod('POST', new apigateway.LambdaIntegration(createPostFn))
+
+    const post = posts.addResource('{postId}')
+    post.addMethod('DELETE', new apigateway.LambdaIntegration(deletePostFn))
+
+    const likes = post.addResource('likes')
+    likes.addMethod('POST', new apigateway.LambdaIntegration(likePostFn))
+    likes.addMethod('DELETE', new apigateway.LambdaIntegration(unlikePostFn))
   }
 }
